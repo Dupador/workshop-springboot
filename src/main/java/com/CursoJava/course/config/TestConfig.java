bb262bfaaf8228cd31ead226c61bacf171523004
Willaim-dias <williampacifico41@gmail.com>
2024-08-28 14:35:26 -0300

    Payment, one-to-one association

--- src/main/java/com/CursoJava/course/config/TestConfig.java
+++ src/main/java/com/CursoJava/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.CursoJava.course.entities.Category;
 import com.CursoJava.course.entities.Order;
 import com.CursoJava.course.entities.OrderItem;
+import com.CursoJava.course.entities.Payment;
 import com.CursoJava.course.entities.Product;
 import com.CursoJava.course.entities.User;
 import com.CursoJava.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
